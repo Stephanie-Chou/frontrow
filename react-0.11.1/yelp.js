94bc1966107cbc6b0063732ce02407932bb0e991
Stephanie Chou <stephanie.chou@outlook.com>
2014-08-31 19:47:10 -0500

    test xmlhttprequest

---
+++ react-0.11.1/yelp.js
@@ -0,0 +1,10 @@
+var request = new XMLHttpRequest();
+request.onreadystatechange = function() {
+
+    if (request.readyState == 4) {
+      JSON.parse(request.response)
+      alert(request.responseText);
+    }
+}
+request.open('GET', "https://maps.googleapis.com/maps/api/geocode/json?address=northbrook,+il&key=AIzaSyAVkmq_gu_UwQiR7znb8Bf0_mktYaEDt0E", true);
+request.send(null);
